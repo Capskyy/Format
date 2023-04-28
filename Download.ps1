269dd1c20edccd2af3934c6ebb80482c44fc08d2
Capskyy <35524294+Capskyy@users.noreply.github.com>
2023-04-29 01:58:55 +0200

    someday I gonna kill someone for this shit...
    I had a cup of tea and now it's fixed

---
+++ Download.ps1
@@ -0,0 +1,100 @@
+echo ""
+echo "Installowanie Vscodium"
+echo ""
+winget install -e --id VSCodium.VSCodium
+echo ""
+echo "Installowanie Krita"
+echo ""
+winget install -e --id KDE.Krita
+echo ""
+echo "Installowanie Gimp"
+echo ""
+winget install -e --id GIMP.GIMP
+echo ""
+echo "Installowanie Vivaldi"
+echo ""
+winget install -e --id VivaldiTechnologies.Vivaldi
+echo ""
+echo "Installowanie Steam"
+echo ""
+winget install -e --id Valve.Steam
+echo ""
+echo "Installowanie Epic Game"
+echo ""
+winget install -e --id EpicGames.EpicGamesLauncher
+echo ""
+echo "Installowanie OBS Studio"
+echo ""
+winget install -e --id OBSProject.OBSStudio
+echo ""
+echo "Installowanie Discord"
+echo ""
+winget install -e --id Discord.Discord
+echo ""
+echo "Installowanie WinDirStat"
+echo ""
+winget install -e --id WinDirStat.WinDirStat
+echo ""
+echo "Installowanie Msi Afterburner"
+echo ""
+winget install -e --id Guru3D.Afterburner
+echo ""
+echo "Installowanie Dysk Google"
+echo ""
+winget install -e --id Google.Drive
+echo ""
+echo "Installowanie Firefox"
+echo ""
+winget install -e --id Mozilla.Firefox
+echo ""
+echo "Installowanie Chrome"
+echo ""
+winget install -e --id Google.Chrome
+echo ""
+echo "Installowanie Audacity"
+echo ""
+winget install -e --id Audacity.Audacity
+echo ""
+echo "Installowanie Curse"
+echo ""
+winget install -e --id Overwolf.CurseForge
+echo ""
+echo "Installowanie LoL"
+echo ""
+winget install -e --id RiotGames.LeagueOfLegends.EUNE
+echo ""
+echo "Installowanie hwinfo"
+echo ""
+winget install -e --id REALiX.HWiNFO
+echo ""
+echo "Installowanie 7Zip"
+echo ""
+winget install -e --id 7zip.7zip
+echo ""
+echo "Installowanie WinSCP"
+echo ""
+winget install -e --id WinSCP.WinSCP
+echo ""
+echo "Installowanie Putty"
+echo ""
+winget install -e --id PuTTY.PuTTY
+echo ""
+echo "Installowanie GeForce Experience"
+echo ""
+winget install -e --id Nvidia.GeForceExperience
+echo ""
+echo "Installowanie Git"
+echo ""
+winget install -e --id Git.Git
+echo ""
+echo "Installowanie Signal"
+echo ""
+winget install -e --id OpenWhisperSystems.Signal
+echo ""
+echo "Installowanie Libre Office"
+echo ""
+winget install -e --id TheDocumentFoundation.LibreOffice
+echo ""
+echo "Koniec Instalacji"
+echo ""
+pause
